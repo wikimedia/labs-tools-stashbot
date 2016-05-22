13ebaa124f34f95081722d88f118a6bdb9dc4a9c
Bryan Davis <bd808@wikimedia.org>
2016-05-21 23:46:18 -0600

    Remove unused imports from phab.py

--- stashbot/phab.py
+++ stashbot/phab.py
@@ -17,10 +17,8 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import hashlib
 import json
 import requests
-import time
 
 
 class Client(object):
