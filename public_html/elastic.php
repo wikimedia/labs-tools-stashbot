97aac2ba23a264bbe2c2752b17960b26fe92b757
Bryan Davis <bd808@wikimedia.org>
2020-04-11 22:05:08 -0600

    Update website bits and pieces
    * toolforge.org URLs!
    * es7 backend
    * Code is in Gerrit these days
    
    Change-Id: I6a79ac7a26d5cc5927b969c939671dea46e70305

--- public_html/elastic.php
+++ public_html/elastic.php
@@ -1,9 +1,5 @@
 <?php
 header('Content-type: text/plain');
-echo file_get_contents('http://tools-elastic-01/?pretty');
+echo file_get_contents('http://elasticsearch.svc.tools.eqiad1.wikimedia.cloud/?pretty');
 echo "\n";
-echo file_get_contents('http://tools-elastic-01/_cluster/health?pretty');
-echo "\n";
-echo file_get_contents('http://tools-elastic-01/_cat/nodes?v&s=name');
-echo "\n";
-echo file_get_contents('http://tools-elastic-01/_cat/indices?v&s=index');
+echo file_get_contents('http://elasticsearch.svc.tools.eqiad1.wikimedia.cloud/_cluster/health?pretty');
